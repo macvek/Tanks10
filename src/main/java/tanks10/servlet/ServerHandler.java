90396271addb18de4110caba91f92a2bb4fe3c74
maleksandrowicz <maleksandrowicz@G3H9N12.SIIPOLSKA.PL>
2014-12-12 16:20:38 +0100

    Added dummy websocket support

---
+++ src/main/java/tanks10/servlet/ServerHandler.java
@@ -0,0 +1,23 @@
+package tanks10.servlet;
+
+import java.io.IOException;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+
+/**
+ *
+ * @author maleksandrowicz
+ */
+
+public class ServerHandler  extends TextWebSocketHandler {
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        System.out.println(message);
+        session.sendMessage(new TextMessage("Response to:"+message));
+    }
+
+}
